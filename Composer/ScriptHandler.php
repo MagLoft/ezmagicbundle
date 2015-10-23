bcd4b2e1b122cb8febc2594c22b3a31c69ff2374
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2015-10-23 13:52:59 +0800

    fix doctrine registry autoload in script runner

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -4,6 +4,7 @@ namespace MagLoft\EzmagicBundle\Composer;
 
 use Composer\Script\Event;
 use MagLoft\EzmagicBundle\Service\EzMagicService;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class ScriptHandler {
     
@@ -12,6 +13,13 @@ class ScriptHandler {
 
     public static function setupEzMagic(Event $event) {
         $io = $event->getIO();
+        $extra = $event->getComposer()->getPackage()->getExtra();
+        
+        // Prepare Doctrine Annotations
+        $rootDir = rtrim(getcwd(), '/') . '/' . trim($extra['symfony-app-dir'], '/');
+        $loader = require_once "$rootDir/../vendor/autoload.php";
+        AnnotationRegistry::registerLoader(array($loader,'loadClass'));
+        
         $container = self::getContainer($event);
         $ezMagicService = new EzMagicService($io, $container);
         $ezMagicService->validate();
